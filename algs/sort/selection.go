abcdb9bdb1ff94d72355a245410ef2345b480b00
Denis Peshkov <denpeshkov98@gmail.com>
2022-10-15 16:11:17 +0300

    Added selection sort

---
+++ algs/sort/selection.go
@@ -0,0 +1,27 @@
+package sort
+
+import "github.com/DenPeshkov/algs-ds/utils"
+
+/* Selection implements selection sort. */
+func Selection[T any](arr []T, cmp utils.Comparator[T]) {
+	N := len(arr)
+
+	for i := 0; i < N; i++ {
+		minInd := min(arr[i:], cmp)
+
+		arr[i], arr[minInd] = arr[minInd], arr[i]
+	}
+}
+
+func min[T any](arr []T, cmp utils.Comparator[T]) (minInd int) {
+	min := arr[0]
+
+	for i, a := range arr[1:] {
+		if cmp(a, min) < 0 {
+			min = a
+			minInd = i
+		}
+	}
+
+	return minInd
+}
