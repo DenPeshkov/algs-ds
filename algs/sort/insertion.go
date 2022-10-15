adc8d08cb13d3bf107839221f83ec48a3611cbfe
Denis Peshkov <denpeshkov98@gmail.com>
2022-10-15 16:11:01 +0300

    Added insertion sort

---
+++ algs/sort/insertion.go
@@ -0,0 +1,25 @@
+/*
+Package sort implements various sorting algortihms.
+*/
+package sort
+
+import (
+	"github.com/DenPeshkov/algs-ds/utils"
+)
+
+// Insertion implements insertion sort.
+func Insertion[T any](arr []T, cmp utils.Comparator[T]) {
+	N := len(arr)
+
+	for i := 1; i < N; i++ {
+		x := arr[i]
+		j := i - 1
+
+		for j >= 0 && cmp(arr[j], x) > 0 {
+			arr[j+1] = arr[j]
+			j--
+		}
+
+		arr[j+1] = x
+	}
+}
