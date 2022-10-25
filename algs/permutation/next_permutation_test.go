14b799682a0706c03e6e19a887db621ce6c35caf
Denis Peshkov <denpeshkov98@gmail.com>
2022-10-25 18:42:11 +0300

    Added unit test for next permutation

---
+++ algs/permutation/next_permutation_test.go
@@ -0,0 +1,39 @@
+package permutation_test
+
+import (
+	. "github.com/DenPeshkov/algs-ds/algs/permutation"
+	"testing"
+)
+
+func TestNext(t *testing.T) {
+	tests := []struct {
+		name string
+		arr  []int
+		want bool
+	}{
+		{"exists", []int{1, 2, 3}, true},
+		{"exists", []int{1, 3, 3}, true},
+		{"not exists", []int{3, 2, 1}, false},
+		{"not exists", []int{1}, false},
+		{"exists", []int{1, 2}, true},
+		{"exists", []int{3, 1, 2, 0, 4}, true},
+		{"not exists", []int{4, 3, 3, 1}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Next(tt.arr, cmpInt); got != tt.want {
+				t.Errorf("Next() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func cmpInt(a, b int) int {
+	if a < b {
+		return -1
+	} else if a == b {
+		return 0
+	} else {
+		return 1
+	}
+}
